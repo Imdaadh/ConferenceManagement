97e0609ea2c9ac6d5eee80c94f32bb08ad7e0e05
ImdaadhIT19148250 <71269158+ImdaadhIT19148250@users.noreply.github.com>
2021-07-04 09:26:36 +0530

    urlchange

--- src/components/workshop/createWorkshop.js
+++ src/components/workshop/createWorkshop.js
@@ -1,66 +1,217 @@
-import React, { PureComponent } from 'react'
-import axios from 'axios'
-import  '../Registration/login.css'
-import Header from "../header/myheader";
-import Footer from "../footer/footer"
+import React, { useState, useRef ,useEffect} from 'react';
+import Dropzone from 'react-dropzone';
+import axios from 'axios';
+import { Form, Row, Col, Button } from 'react-bootstrap';
+import Header from '../header/myheader'
+import {useParams} from 'react-router-dom'
+import decode from "jwt-decode";
 import {API_URL} from "../utils/url";
+import  '../Registration/login.css'
 
-class CreateWorkshop extends PureComponent {
-    constructor(props) {
-        super(props)
-        this.onChange = this.onChange.bind(this);
-        this.onSubmit=this.onSubmit.bind(this);
-        this.state = {
-            name:'',
-            university:'',
-            mobileNumber:'',
-            topic:'',
-            statementOfIntrest:''
-        }
-    }
-    onChange(e) {
-        this.setState({ [e.target.name]: e.target.value })
-    }
-    onSubmit(e){
-        e.preventDefault();
-        try {
-            const email =this.props.match.params.email;
-            const workshops={
-                name:this.state.name,
-                university:this.state.university,
-                mobileNumber:this.state.mobileNumber,
-                topic:this.state.topic,
-                statementOfIntrest:this.state.statementOfIntrest,
-                contactDetail:email
+
+const CreateWorkshop = (props) => {
+    const [file, setFile] = useState(null); // state for storing actual image
+    const [previewSrc, setPreviewSrc] = useState(''); // state for storing previewImage
+    const [email, setEmail] = useState('');
+    const params = useParams()
+    const [state, setState] = useState({
+        title:'',
+        university:'',
+        topic:'',
+        contactDetails:''
+
+
+
+    });
+    const [errorMsg, setErrorMsg] = useState('');
+    const [isPreviewAvailable, setIsPreviewAvailable] = useState(false); // state to show preview only for images
+    const dropRef = useRef(); // React ref for managing the hover state of droppable area
+
+    ///////
+    useEffect(() => {
+        const getFilesList = async () => {
+            try {
+                if(sessionStorage.token){
+                    // this.setState({email:decode(sessionStorage.token).email})
+                    setEmail(decode(sessionStorage.token).email)
                 }
-                console.log(workshops);
-                axios.post('https://afprojectconference.herokuapp.com/workshop/addWorkshop',workshops).then(res => alert(res.data.msg))
-                  this.props.history.push('/')
-        } catch (error) {
-            alert(error.response.data.msg)
-        }
-    }
-    render() {
-        return (
-            <div>
-                <Header   />
-                <div className="login-page"    style={{  height: 700, marginTop:150}}>
-                      <h1>Add Workshop</h1>
-                      <form  onSubmit={this.onSubmit}>
-                          <input type='text'  placeholder="Name"  name="name" value={this.state.name} onChange={this.onChange} required={true} />
-                          <input type='text'  placeholder="university"  name="university"  value={this.state.university}  onChange={this.onChange}required={true}/>
-                          <input type='number'  placeholder="mobileNumber"  name="mobileNumber"  value={this.state.mobileNumber}  onChange={this.onChange}required={true}/>
-                          <input type='text'   placeholder="topic" name="topic" value={this.state.topic} onChange={this.onChange}required={true} />
-                          <input type='description'  placeholder="statementOfIntrest" name="statementOfIntrest"  value={this.state.statementOfIntrest}  onChange={this.onChange}required={true}/>
-                          <div className="row">
-                              <button type="submit">submit</button>
-                          </div>
-                      </form>
-                </div>
-                <Footer/>
-            </div>
-        )
-    }
-}
 
-export default CreateWorkshop
+            } catch (error) {
+                error.response && setErrorMsg(error.response.data);
+            }
+        };
+        getFilesList();
+    }, []);
+    /////////
+
+
+
+
+    const handleInputChange = (event) => {
+        setState({
+            ...state,
+            [event.target.name]: event.target.value
+        });
+    };
+
+    const onDrop = (files) => {
+        const [uploadedFile] = files;
+        setFile(uploadedFile);
+
+        const fileReader = new FileReader();
+        fileReader.onload = () => {
+            setPreviewSrc(fileReader.result);
+        };
+        fileReader.readAsDataURL(uploadedFile);
+        setIsPreviewAvailable(uploadedFile.name.match(/\.(jpeg|jpg|png)$/));
+        dropRef.current.style.border = '2px dashed #e9ebeb';
+    };
+
+    const updateBorder = (dragState) => {
+        if (dragState === 'over') {
+            dropRef.current.style.border = '2px solid #000';
+        } else if (dragState === 'leave') {
+            dropRef.current.style.border = '2px dashed #e9ebeb';
+        }
+    };
+
+    const handleOnSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const { title ,university,topic,contactDetails} = state;
+            if (title.trim() !== '' && university.trim() !== '') {
+                if (file) {
+                    const formData = new FormData();
+                    formData.append('file', file);
+                    formData.append('title', title);
+                    formData.append('university', university);
+                    formData.append('topic', topic);
+                    formData.append('contactDetails', contactDetails);
+
+                    setErrorMsg('');
+                    await axios.post(`https://conference-app-af.herokuapp.com/workshop/addWorkshop`, formData, {
+                        headers: {
+                            'Content-Type': 'multipart/form-data'
+                        }
+                    }).then(res => alert(res.data.msg))
+                    props.history.push('/userViewWorkshop');
+                } else {
+                    setErrorMsg('Please select a file to add.');
+                }
+            } else {
+                setErrorMsg('Please enter all the field values.');
+            }
+        } catch (error) {
+            error.response && setErrorMsg(error.response.data);
+        }
+    };
+
+    return (
+        <React.Fragment>
+            <Header />
+            <div className="login-page"    style={{  height: 700, marginTop:150}}>
+                <Form className="search-form" onSubmit={handleOnSubmit} style={{  }}>
+                    {errorMsg && <p className="errorMsg">{errorMsg}</p>}
+                    <Row>
+                        <Col>
+                            <Form.Group controlId="title" style={{width:500}}>
+                                <Form.Control
+                                    type="text"
+                                    name="title"
+                                    value={state.title || ''}
+                                    placeholder="Enter title"
+                                    onChange={handleInputChange}
+                                />
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Form.Group controlId="university" style={{width:500}}>
+                                <Form.Control
+                                    type="text"
+                                    name="university"
+                                    value={state.university || ''}
+                                    placeholder="Enter university"
+                                    onChange={handleInputChange}
+                                />
+                            </Form.Group>
+                        </Col>
+                    </Row>
+
+
+                    <Row>
+                        <Col>
+                            <Form.Group controlId="topic" style={{width:500}}>
+                                <Form.Control
+                                    type="text"
+                                    name="topic"
+                                    value={state.topic || ''}
+                                    placeholder="Enter topic"
+                                    onChange={handleInputChange}
+                                />
+                            </Form.Group>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col>
+                            <Form.Group controlId="contactDetails" style={{width:500}}>
+                                <Form.Control
+                                    type="text"
+                                    name="contactDetails"
+                                    value={state.contactDetails || ''}
+                                    placeholder="Enter contactDetails"
+                                    onChange={handleInputChange}
+                                />
+                            </Form.Group>
+                        </Col>
+                    </Row>
+
+
+
+                    <div className="upload-section">
+                        <Dropzone
+                            onDrop={onDrop}
+                            onDragEnter={() => updateBorder('over')}
+                            onDragLeave={() => updateBorder('leave')}
+                        >
+                            {({ getRootProps, getInputProps }) => (
+                                <div {...getRootProps({ className: 'drop-zone' })} ref={dropRef}>
+                                    <input {...getInputProps()} />
+                                    <p style={{backgroundColor:'lightgray',width:500,height:100}}>Drag and drop a file OR click here to select a file</p>
+                                    {file && (
+                                        <div>
+                                            <strong>Selected file:</strong> {file.name}
+                                        </div>
+                                    )}
+                                </div>
+                            )}
+                        </Dropzone>
+                        {previewSrc ? (
+                            isPreviewAvailable ? (
+                                <div className="image-preview">
+                                    <img className="preview-image" src={previewSrc} alt="Preview" />
+                                </div>
+                            ) : (
+                                <div className="preview-message">
+                                    <p>No preview available for this file</p>
+                                </div>
+                            )
+                        ) : (
+                            <div className="preview-message">
+                                <p>Image preview will be shown here after selection</p>
+                            </div>
+                        )}
+
+                    </div>
+                    <Button variant="primary" type="submit" style={{width:300,marginRight:1000}}>
+                        Submit
+                    </Button>
+                </Form></div>
+        </React.Fragment>
+    );
+};
+
+export default CreateWorkshop;
